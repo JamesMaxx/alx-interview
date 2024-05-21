260ebfe8f5b441147498e2462c76aace5772402f
JamesMaxx <jamesmaxmunene@gmail.com>
2024-05-21 12:08:51 +0300

    chore: Refactor log parsing script for improved readability and efficiency

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,67 +1,54 @@
 #!/usr/bin/python3
 
-import re
 import sys
 
 
-def extract_input(input_line):
-    pattern = (
-        r'\s*(?P<ip>\S+)\s*'
-        r'\-\s*\[\s*(?P<date>\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2}\.\d+)\]\s*'
-        r'"GET\s+/\S+\s+HTTP/\d+\.\d+"\s*'
-        r'(?P<status_code>\d+)\s*'
-        r'(?P<file_size>\d+)'
-    )
-    match = re.fullmatch(pattern, input_line)
-    if match:
-        return {
-            'status_code': match.group('status_code'),
-            'file_size': int(match.group('file_size')),
-        }
-    return None
+def print_msg(dict_sc, total_file_size):
+    """
+    Method to print
+    Args:
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
+    """
+
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
 
-def print_statistics(total_file_size, status_codes_stats):
-    print(f'File size: {total_file_size}')
-    for status_code in sorted(status_codes_stats.keys()):
-        num = status_codes_stats.get(status_code, 0)
-        if num > 0:
-            print(f'{status_code}: {num}')
+total_file_size = 0
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
 
-def update_metrics(line, total_file_size, status_codes_stats):
-    line_info = extract_input(line)
-    if line_info:
-        status_code = line_info['status_code']
-        file_size = line_info['file_size']
-        total_file_size += file_size
-        status_codes_stats[status_code] = status_codes_stats.get(status_code, 0) + 1
-    return total_file_size
+        if len(parsed_line) > 2:
+            counter += 1
 
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
 
-def run():
-    line_num = 0
-    total_file_size = 0
-    status_codes_stats = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0,
-    }
-    try:
-        for line in sys.stdin:
-            line_num += 1
-            total_file_size = update_metrics(line, total_file_size, status_codes_stats)
-            if line_num % 10 == 0:
-                print_statistics(total_file_size, status_codes_stats)
-    except KeyboardInterrupt:
-        print_statistics(total_file_size, status_codes_stats)
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
 
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
 
-if __name__ == '__main__':
-    run()
-
+finally:
+    print_msg(dict_sc, total_file_size)
