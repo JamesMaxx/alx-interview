7df16fecee1cd84151c1ad378a383d21e88f81a2
SlimjimmyMax <slimjimmymax@outlook.com>
2024-05-24 16:38:13 +0300

    Refactor UTF-8 validation function for improved efficiency and readability

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
-def validUTF8(data):
+"""
+UTF-8 Validation
+"""
+
+
+def validUTF8(data) -> bool:
     """
-    Determines if a given dataset represents a valid UTF-8 encoding
-
-    Parameters:
-    data: List of integers
-
-    Returns:
-    True if data is a valid UTF-8 encoding, else return False
+    Returns True if data is a valid UTF-8 encoding, else return False
+    :param data:
+    :return:
     """
-    n_bytes = 0
-
-    mask1 = 1 << 7
-    mask2 = 1 << 6
-    for num in data:
-
+    num_bytes = 0
+    for byte in data:
         mask = 1 << 7
-        if n_bytes == 0:
-            while mask & num:
-                n_bytes += 1
-                mask = mask >> 1
-
-            if n_bytes == 0:
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
                 continue
-
-            if n_bytes == 1 or n_bytes > 4:
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            if not (num & mask1 and not (num & mask2)):
+            if byte >> 6 != 0b10:
                 return False
-        n_bytes -= 1
-
-    return n_bytes == 0
+        num_bytes -= 1
+    return num_bytes == 0
