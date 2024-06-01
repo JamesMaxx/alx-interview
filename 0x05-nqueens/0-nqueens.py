3a33fcc9195bf9fb672b69d24725e2ffa2c09185
SlimjimmyMax <slimjimmymax@outlook.com>
2024-06-01 13:37:25 +0300

    N-queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -21,34 +21,67 @@ if n_q < 4:
 
 
 def solve_nqueens(n):
-    '''self descriptive'''
-    if n == 0:
-        return [[]]
-    inner_solution = solve_nqueens(n - 1)
-    return [solution + [(n, i + 1)]
-            for i in range(n_q)
-            for solution in inner_solution
-            if safe_queen((n, i + 1), solution)]
+    """
+    Solves the N-Queens problem using backtracking.
+    Returns a list of lists, where each inner list represents a solution
+    with the positions of the queens on the board.
+    """
+    def is_safe(board, row, col):
+        """
+        Checks if a queen can be placed on the board[row][col].
+        """
+        # Check this row on the left side
+        for i in range(col):
+            if board[row][i] == 1:
+                return False
+
+        # Check upper diagonal on the left side
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+
+        # Check lower diagonal on the left side
+        for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+
+        return True
+
+    def solve(board, col):
+        """
+        Recursive function to solve the N-Queens problem.
+        """
+        # Base case: If all queens are placed, return True
+        if col == n:
+            solutions.append([list(board[i]) for i in range(n)])
+            return True
+
+        # Place this queen in all rows one by one
+        res = False
+        for i in range(n):
+            # If a queen can be placed on the board[i][col]
+            if is_safe(board, i, col):
+                # Place this queen on board[i][col]
+                board[i][col] = 1
+
+                # Recur to place rest of the queens
+                res = solve(board, col + 1) or res
+
+                # If placing the queen in board[i][col] doesn't lead to a solution,
+                # remove the queen from board[i][col]
+                board[i][col] = 0
+
+        # If the queen cannot be placed in any row in this column, return False
+        return res
+
+    solutions = []
+    board = [[0 for j in range(n)] for i in range(n)]
+    solve(board, 0)
+    return solutions
 
 
-def attack_queen(square, queen):
-    '''self descriptive'''
-    (row1, col1) = square
-    (row2, col2) = queen
-    return (row1 == row2) or (col1 == col2) or\
-        abs(row1 - row2) == abs(col1 - col2)
-
-
-def safe_queen(sqr, queens):
-    '''self descriptive'''
-    for queen in queens:
-        if attack_queen(sqr, queen):
-            return False
-    return True
-
-
-for answer in reversed(solve_nqueens(n_q)):
+for answer in solve_nqueens(n_q):
     result = []
-    for p in [list(p) for p in answer]:
-        result.append([i - 1 for i in p])
+    for p in answer:
+        result.append([i + 1 for i, x in enumerate(p) if x == 1])
     print(result)
