538c7f6e9ae5d95cf0b7c31f78e298649270c594
Jamesmax Macharia <jamesmaxmunene@gmail.com>
2024-06-27 16:27:27 +0300

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,44 +1,25 @@
+#!/usr/bin/python3
+"""Island perimeter computing module.
+"""
+
+
 def island_perimeter(grid):
-    """
-    Returns the perimeter of the island described in grid.
-    
-    Args:
-        grid (list of list of integers): 2D grid where 0 represents water, 
-                                         1 represents land.
-    
-    Returns:
-        int: perimeter of the island.
+    """Computes the perimeter of an island with no lakes.
     """
     perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
-    
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                perimeter += _count_perimeter(grid, row, col)
-    return perimeter
-
-
-def _count_perimeter(grid, row, col):
-    """
-    Counts the perimeter of an island.
-    
-    Args:
-        grid (list of list of integers): 2D grid where 0 represents water, 
-                                         1 represents land.
-        row (int): row index of the island.
-        col (int): column index of the island.
-    
-    Returns:
-        int: perimeter of the island.
-    """
-    perimeter = 4
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    
-    for dr, dc in directions:
-        if (row + dr >= 0 and row + dr < len(grid)
-            and col + dc >= 0 and col + dc < len(grid[0])
-            and grid[row + dr][col + dc] == 0):
-            perimeter -= 1
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
     return perimeter
